13e2f238502389a6cdab18861e1072e69ec011d7
shaktipawar <shakti.pawar@outlook.com>
2025-05-14 14:05:06 +0530

    Updated the syntax to merge values in tags attribute

--- 01-website/main.tf
+++ 01-website/main.tf
@@ -14,11 +14,10 @@ resource "aws_vpc" "vpc" {
     instance_tenancy = var.instance_tenancy
     enable_dns_support = var.enable_dns_support
     enable_dns_hostnames = var.enable_dns_hostnames
-    tags = "${merge(
+    tags = merge(
                 var.vpc_tags, 
                 local.default_tags, 
                 map(
                     "Name", "vpc-${var.product_name}-${var.environment}")
                 )
-            }"
 }
